7bf621e121f393198a3d5c2a08062bfe883c07cc
Sudharsan K <sudharsan_1985@live.in>
2022-05-20 18:18:58 +0800

    Created the listview control

---
+++ demo/src/webparts/listViewControl/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IListViewControlWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ListViewControlWebPartStrings' {
+  const strings: IListViewControlWebPartStrings;
+  export = strings;
+}
